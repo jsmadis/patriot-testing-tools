d830e8ae96450760eff90e7d176f6b513b528f3e
Jakub <jakub.smadis@gmail.com>
2021-03-30 20:37:53 +0200

    Remake get endpoint to post, to be able send request body

--- patriot-http-coap-testing-app/src/main/java/com/example/restservice/HttpToCoapController.java
+++ patriot-http-coap-testing-app/src/main/java/com/example/restservice/HttpToCoapController.java
@@ -3,9 +3,8 @@ package com.example.restservice;
 
 import io.patriot_framework.generator.controll.client.CoapControlClient;
 import org.eclipse.californium.elements.exception.ConnectorException;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -13,7 +12,7 @@ import java.io.IOException;
 @RestController
 public class HttpToCoapController {
 
-    @GetMapping("/get")
+    @PostMapping("/get")
     public String get(@RequestBody() CoapConfiguration coapConfiguration) throws ConnectorException, IOException {
         CoapControlClient client = new CoapControlClient(coapConfiguration.getHostname());
         return new String(client.get(coapConfiguration.getPath()).getPayload());
